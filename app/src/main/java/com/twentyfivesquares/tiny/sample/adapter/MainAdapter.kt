6ac971cdc11cec0fed452337460218e023ff3756
jordanbeck <jordan.s.beck@gmail.com>
2017-01-08 12:49:02 -0500

    Adding the list adapter.

---
+++ app/src/main/java/com/twentyfivesquares/tiny/sample/adapter/MainAdapter.kt
@@ -0,0 +1,28 @@
+package com.twentyfivesquares.tiny.sample.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.twentyfivesquares.tiny.sample.R
+import kotlinx.android.synthetic.main.item_list.view.*
+
+class MainAdapter(private var items: List<String>) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent!!.context).inflate(R.layout.item_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder?.vName?.text = items[position]
+    }
+
+    override fun getItemCount(): Int {
+        return if (items.isEmpty()) 0 else items.count()
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val vName by lazy { itemView.item_name }
+    }
+}
--- app/src/main/java/com/twentyfivesquares/tiny/sample/controller/MainController.kt
+++ app/src/main/java/com/twentyfivesquares/tiny/sample/controller/MainController.kt
@@ -1,11 +1,22 @@
 package com.twentyfivesquares.tiny.sample.controller
 
 import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
 import com.twentyfivesquares.tiny.TinyController
 import com.twentyfivesquares.tiny.sample.R
+import com.twentyfivesquares.tiny.sample.adapter.MainAdapter
+import kotlinx.android.synthetic.main.controller_main.view.*
+import java.util.*
 
 class MainController(context: Context?) : TinyController(context) {
 
-    override fun getLayoutRes(): Int = R.layout.controller_main
+    private val vRecycler by lazy { view.main_recycler }
 
+    init {
+        val items = Arrays.asList("Item 1", "Item 2", "Item 3")
+        vRecycler.layoutManager = LinearLayoutManager(context)
+        vRecycler.adapter = MainAdapter(items)
+    }
+
+    override fun getLayoutRes(): Int = R.layout.controller_main
 }
